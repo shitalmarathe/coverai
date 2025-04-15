818e17c291f0798c51c84472c0faa926b23dc6fd
shitalmarathe <shitalmarathe57@gmail.com>
2025-04-15 11:45:07 +0000

    Create first validation in server action for user registration

--- actions/userController.js
+++ actions/userController.js
@@ -1,5 +1,33 @@
 "use server";
- 
- export const register = async function () {
-   console.log("Function triggered!");
- };
+
+export const register = async function (prevState, formData) {
+  const errors = [];
+
+  const data = {
+    username: formData.get("username"),
+    password: formData.get("password"),
+  };
+
+  console.log(data);
+
+  //  Step 1: Validate the incoming user data
+  if (typeof data.username !== "string") data.username = "";
+  if (typeof data.password !== "string") data.password = "";
+
+  data.username = data.username.trim();
+
+  if (data.username.length < 3)
+    errors.push("Username must be atleast 3 characters");
+
+  if (errors.length) {
+    return {
+      errors: errors,
+      success: false,
+    };
+  }
+
+  //  Step 2: Save the user in to our database
+  //  Step 3: Send a cookie back for login session
+
+  return { success: true };
+};
--- app/page.jsx
+++ app/page.jsx
@@ -1,54 +1,57 @@
-import Link from "next/link";
+"use client";
 
+import { useActionState } from "react";
 import { register } from "@/actions/userController";
 
-
 export default function Page() {
+  const [state, formAction] = useActionState(register, {});
+
+  console.log(state);
+
   return (
     <div className="min-h-[85vh] container mx-auto grid grid-cols-1 md:grid-cols-2 px-3">
-       <div className="flex items-center">
-         <h1 className="text-amber-400 text-3xl md:text-4xl font-light tracking-wide">
-           CoverAI &mdash; Create Engaging Thumbnails <br />
-           using GenAI
-         </h1>
-       </div>
- 
-       {/* Register Form */}
-       <div className="flex items-start md:items-center">
-         <form
-           action="#"
-           method="POST"
-           className="w-full flex flex-col items-start"
-         >
-           <input
-             type="text"
-             name="username"
-             placeholder="Username"
-             className="input"
-             autoComplete="off"
-             required
-           />
-           <br />
- 
-           <input
-             type="password"
-             name="password"
-             placeholder="Password"
-             className="input"
-             autoComplete="off"
-             required
-           />
-           <br />
- 
-           <button
-             type="submit"
-             className="btn"
-           >
-             Register
-           </button>
-         </form>
-       </div>
-    
+      <div className="flex items-center">
+        <h1 className="text-amber-400 text-3xl md:text-4xl font-light tracking-wide">
+          CoverAI &mdash; Create Engaging Thumbnails <br />
+          using GenAI
+        </h1>
+      </div>
+
+      {/* Register Form */}
+      <div className="flex items-start md:items-center">
+        <form
+          action={formAction}
+          method="POST"
+          className="w-full flex flex-col items-start"
+        >
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            className="input"
+            autoComplete="off"
+            required
+          />
+          <br />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="input"
+            autoComplete="off"
+            required
+          />
+          <br />
+
+          <button
+            type="submit"
+            className="btn"
+          >
+            Register
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
