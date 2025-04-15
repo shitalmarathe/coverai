df492fec654dcce57900e0198f9a7ead6f3cce99
shitalmarathe <shitalmarathe57@gmail.com>
2025-04-15 10:59:48 +0000

    fix changes

---
+++ app/login/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <h3>Login page</h3>
+      <Link href="/">Go back to home</Link>
+    </div>
+  );
+}
